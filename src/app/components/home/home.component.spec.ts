101feb1f1bf2b5a5d0c05c6da6cbf1b39207b043
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2024-01-09 17:10:17 +0100

    Added tests for home component - added tests for home component - added tests for search component

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { HackernewsService } from '../../shared/services/hackernews.service';
+import { StoreService } from '../../shared/services/store.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let hackernewsService: HackernewsService;
+  let storeService: StoreService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
-    
+      providers: [HackernewsService, StoreService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    hackernewsService = TestBed.inject(HackernewsService);
+    storeService = TestBed.inject(StoreService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call freshSearch method on ngOnInit', async () => {
+    spyOn(component, 'freshSearch');
+    await component.ngOnInit();
+    expect(component.freshSearch).toHaveBeenCalled();
+  });
+
+  it('should set search query to empty string on freshSearch', async () => {
+    spyOn(storeService, 'setSearchQuery');
+    await component.freshSearch();
+    expect(storeService.setSearchQuery).toHaveBeenCalledWith('');
+  });
+
+  it('should call updateHackerNewsRes method on freshSearch', async () => {
+    spyOn(hackernewsService, 'updateHackerNewsRes');
+    await component.freshSearch();
+    expect(hackernewsService.updateHackerNewsRes).toHaveBeenCalledWith('');
   });
 });
--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
+import { Subject } from 'rxjs';
+import { HackernewsService } from '../../shared/services/hackernews.service';
+import { StoreService } from '../../shared/services/store.service';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let hackernewsService: HackernewsService;
+  let storeService: StoreService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
-    })
-    .compileComponents();
-    
+      imports: [SearchComponent],
+      providers: [HackernewsService, StoreService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+    hackernewsService = TestBed.inject(HackernewsService);
+    storeService = TestBed.inject(StoreService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call performSearch and toggleSearchShowing methods when search method is called', async () => {
+    const performSearchSpy = spyOn(component, 'performSearch').and.callThrough();
+    const toggleSearchShowingSpy = spyOn(storeService, 'toggleSearchShowing');
+    await component.search();
+    expect(performSearchSpy).toHaveBeenCalled();
+    expect(toggleSearchShowingSpy).toHaveBeenCalled();
   });
 });
